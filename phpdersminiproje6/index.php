07692f95acf80cfad27280e969b6d2fe1db2756b
Özgür Karakuş <103831584+Aagonn@users.noreply.github.com>
2023-09-17 15:57:40 +0300

    Add files via upload

---
+++ phpdersminiproje6/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta http-equiv="Content-Type"content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Languange"content="tr">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Super Global</title>
+</head>
+
+<body>
+    <form action="sonuc.php" method="post" enctype="multipart/form-data">
+        Seçiniz: <input type="file" name="Dosya">
+        <input type="submit" value="Dosyayı Yükle">
+</body>
+
+</html>
